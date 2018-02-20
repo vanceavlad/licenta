f1c647dfcb28a10f3722c1597779f53ac05b602b
Vlad Vancea <vavl1996>
2018-02-20 19:41:27 +0200

    Added general errors page

--- src/main/java/com/licenta/controller/ErrorController.java
+++ src/main/java/com/licenta/controller/ErrorController.java
@@ -20,4 +20,45 @@ public class ErrorController {
 
         return errorPage;
     }
+
+
+    @RequestMapping(value = "errorsPage", method = RequestMethod.GET)
+    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
+        ModelAndView errorPage = new ModelAndView("errorsPage");
+        String errorMsg = "";
+        int httpErrorCode = getErrorCode(httpRequest);
+        switch (httpErrorCode) {
+            case 400: {
+                errorMsg = "Http Error Code: 400. Bad Request.";
+                break;
+            }
+            case 401: {
+                errorMsg = "Http Error Code: 401. Unauthorized.";
+                break;
+            }
+            case 404: {
+                errorMsg = "Http Error Code: 404. Resource not found." +'\n';
+                errorMsg += "Please check the URL to be correct";
+                break;
+            }
+            case 405: {
+                errorMsg ="Http Error Code : 405. Failed to load resource." + System.lineSeparator();
+                errorMsg += "The method specified in the Request-Line is not allowed for the resource identified by the Request-URI." + System.lineSeparator();
+                errorMsg += "Please check the URL to be correct. " + System.lineSeparator();
+                System.out.println(errorMsg);
+                break;
+            }
+            case 500: {
+                errorMsg = "Http Error Code: 500. Internal Server Error.";
+                break;
+            }
+        }
+        errorPage.addObject("errorMsg", errorMsg);
+        return errorPage;
+    }
+
+    private int getErrorCode(HttpServletRequest httpRequest) {
+        return (Integer) httpRequest
+                .getAttribute("javax.servlet.error.status_code");
+    }
 }
